e70b67c68fba658d8190299a6049212a87b238f9
mkokonyan <mkokonyan@gmail.com>
2021-11-14 22:24:00 +0200

    commit

--- 02_tuples_and_sets/b_excercise/05_longest_interseciton.py
+++ 02_tuples_and_sets/b_excercise/05_longest_interseciton.py
@@ -1,19 +1,34 @@
+# n = int(input())
+# range_1 = set()
+# range_2 = set()
+# max_interesting_lenght = 0
+# intersection_range = set()
+# for i in range(n):
+#     first_range, second_range = input().split("-")
+#     first_start, first_end = first_range.split(",")
+#     second_start, second_end = second_range.split(",")
+#     for r1 in range(int(first_start), int(first_end)+1):
+#         range_1.add(r1)
+#     for r2 in range(int(second_start), int(second_end)+1):
+#         range_2.add(r2)
+#     if len(range_1.intersection(range_2)) >= max_interesting_lenght:
+#         max_interesting_lenght = len(range_1.intersection(range_2))
+#         intersection_range = range_1.intersection(range_2)
+#     range_1.clear()
+#     range_2.clear()
+# print(f"Longest intersection is [{', '.join(list(map(str,intersection_range)))}] with length {max_interesting_lenght}")
+
 n = int(input())
-range_1 = set()
-range_2 = set()
-max_interesting_lenght = 0
-intersection_range = set()
-for i in range(n):
-    first_range, second_range = input().split("-")
-    first_start, first_end = first_range.split(",")
-    second_start, second_end = second_range.split(",")
-    for r1 in range(int(first_start), int(first_end)+1):
-        range_1.add(r1)
-    for r2 in range(int(second_start), int(second_end)+1):
-        range_2.add(r2)
-    if len(range_1.intersection(range_2)) >= max_interesting_lenght:
-        max_interesting_lenght = len(range_1.intersection(range_2))
-        intersection_range = range_1.intersection(range_2)
-    range_1.clear()
-    range_2.clear()
-print(f"Longest intersection is [{', '.join(list(map(str,intersection_range)))}] with length {max_interesting_lenght}")
+
+intersections = []
+for _ in range(n):
+    data = input()
+    first_seq_data, second_seq_data = data.split("-")
+    start, stop = [int(el) for el in first_seq_data.split(",")]
+    first_seq = range(start, stop+1)
+    start, stop = [int(el) for el in second_seq_data.split(",")]
+    second_seq = range(start, stop+1)
+    intersection = set(first_seq).intersection(second_seq)
+    intersections.append(intersection)
+longest = sorted(intersections, key=lambda x: -len(x))[0]
+print(f"Longest intersection is {list(longest)} with length {len(longest)}")
--- 02_tuples_and_sets/b_excercise/06_battle_of_names.py
+++ 02_tuples_and_sets/b_excercise/06_battle_of_names.py
@@ -1,20 +1,42 @@
-lines = int(input())
-odd_set = set()
-even_set = set()
-for i in range(1, lines + 1):
-    name = input()
-    name_sum = 0
-    for ch in name:
-        name_sum += ord(ch)
-    name_sum //= i
-    if name_sum % 2 == 0:
-        even_set.add(name_sum)
-    else:
-        odd_set.add(name_sum)
+# lines = int(input())
+# odd_set = set()
+# even_set = set()
+# for i in range(1, lines + 1):
+#     name = input()
+#     name_sum = 0
+#     for ch in name:
+#         name_sum += ord(ch)
+#     name_sum //= i
+#     if name_sum % 2 == 0:
+#         even_set.add(name_sum)
+#     else:
+#         odd_set.add(name_sum)
+#
+# if sum(odd_set) == sum(even_set):
+#     print(f"{', '.join(list(map(str, odd_set.union(even_set))))}")
+# elif sum(odd_set) > sum(even_set):
+#     print(f"{', '.join(list(map(str, odd_set.difference(even_set))))}")
+# elif sum(odd_set) < sum(even_set):
+#     print(f"{', '.join(list(map(str, odd_set.symmetric_difference(even_set))))}")
 
-if sum(odd_set) == sum(even_set):
-    print(f"{', '.join(list(map(str, odd_set.union(even_set))))}")
-elif sum(odd_set) > sum(even_set):
-    print(f"{', '.join(list(map(str, odd_set.difference(even_set))))}")
-elif sum(odd_set) < sum(even_set):
-    print(f"{', '.join(list(map(str, odd_set.symmetric_difference(even_set))))}")
+n = int(input())
+
+even_numbers_set = set()
+odd_numbers_set = set()
+for i in range(1, n+1):
+    name = input()
+    current_sum = sum([ord(el) for el in name]) // i
+    if current_sum % 2 == 0:
+        even_numbers_set.add(current_sum)
+    else:
+        odd_numbers_set.add(current_sum)
+
+sum_evens = sum(even_numbers_set)
+sum_odds = sum(odd_numbers_set)
+
+if sum_evens == sum_odds:
+    print(f"{', '.join([str(el) for el in even_numbers_set.union(odd_numbers_set)])}")
+elif sum_odds > sum_evens:
+    print(f"{', '.join([str(el) for el in odd_numbers_set.difference(even_numbers_set)])}")
+else:
+    print(f"{', '.join([str(el) for el in odd_numbers_set.symmetric_difference(even_numbers_set)])}")
