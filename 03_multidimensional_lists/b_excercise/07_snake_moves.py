a739933a3897064120aa5b6b3e68b60b597ee3b8
mkokonyan <mkokonyan@gmail.com>
2021-11-18 00:36:58 +0200

    last commit

--- 03_multidimensional_lists/b_excercise/07_snake_moves.py
+++ 03_multidimensional_lists/b_excercise/07_snake_moves.py
@@ -7,43 +7,30 @@ for row in range(rows):
         matrix[row].append(0)
 
 for row in range(rows):
-    if row == 0:
-        if len(text) > cols:
+    if len(text) >= cols:
+        if row % 2 == 0:
             for col in range(cols):
                 matrix[row][col] = text[col]
             text = text[cols:] + text[:cols]
         else:
-            for col in range(len(text)):
-                matrix[row][col] = text[col]
-            for col in range(len(text), cols):
-                matrix[row][col] = text[col-len(text)]
-            text = text[cols - (len(text)):] + text[:cols - (len(text))]
-    elif row % 2 == 0:
-        if len(text) > cols:
-            for col in range(cols):
-                matrix[row][col] = text[col+(len(text)-cols)]
-            text = text[cols:] + text[:cols]
-        else:
-            for col in range(len(text)):
-                matrix[row][col] = text[col]
-            for col in range(len(text), cols):
-                matrix[row][col] = text[col-len(text)]
-            text = text[cols - (len(text)):] + text[:cols - (len(text))]
-
-
-    else:
-        text = text[::-1]
-        if len(text) > cols:
             for col in range(cols-1, -1, -1):
                 matrix[row][col] = text[col+(len(text)-cols)]
-            text = text[:len(text)-cols][::-1] + text[(len(text)-cols):][::-1]
-        else:
+            text = text[(len(text)-cols):] + text[:len(text)-cols]
+        text = text[::-1]
+    else:
+        if row % 2 == 0:
             for col in range(len(text)):
                 matrix[row][col] = text[col]
             for col in range(len(text), cols):
-                matrix[row][col] = text[col-len(text)]
-            text = text[cols - (len(text)):] + text[:cols - (len(text))]
-
+                matrix[row][col] = text[col - len(text)]
+            text = text[cols-len(text):] + text[:cols-len(text)]
+        else:
+            for col in range(cols-1, cols - len(text), -1):
+                matrix[row][col] = text[col+(len(text)-cols)]
+            for col in range(cols - len(text), -1, -1):
+                matrix[row][col] = text[col+(len(text)-cols)]
+            text = text[(len(text)-cols):] + text[:len(text)-cols]
+        text = text[::-1]
 
 for i in range(rows):
     print("".join(matrix[i]))
---
+++ 03_multidimensional_lists/b_excercise/08_bombs.py
@@ -0,0 +1,50 @@
+def is_valid(index1, index2):
+    if 0 <= index1 < rows and 0 <= index2 < rows and matrix[index1][index2] > 0:
+        return True
+    return False
+
+
+rows = int(input())
+matrix = []
+
+for row in range(rows):
+    matrix.append([int(el) for el in input().split()])
+
+data = [el for el in input().split()]
+for i in range(len(data)):
+    current_row, current_column = [int(x) for x in data[i].split(",")]
+    exploded_cell = matrix[current_row][current_column]
+
+    if is_valid(current_row - 1, current_column):
+        matrix[current_row - 1][current_column] -= exploded_cell
+    if is_valid(current_row - 1, current_column - 1):
+        matrix[current_row - 1][current_column - 1] -= exploded_cell
+    if is_valid(current_row - 1, current_column + 1):
+        matrix[current_row - 1][current_column + 1] -= exploded_cell
+
+    if is_valid(current_row, current_column - 1):
+        matrix[current_row][current_column - 1] -= exploded_cell
+    if is_valid(current_row, current_column + 1):
+        matrix[current_row][current_column + 1] -= exploded_cell
+
+    if is_valid(current_row + 1, current_column):
+        matrix[current_row + 1][current_column] -= exploded_cell
+    if is_valid(current_row + 1, current_column - 1):
+        matrix[current_row + 1][current_column - 1] -= exploded_cell
+    if is_valid(current_row + 1, current_column + 1):
+        matrix[current_row + 1][current_column + 1] -= exploded_cell
+
+    matrix[current_row][current_column] = 0
+
+alive_cells = 0
+sum_active_cells = 0
+
+for i in range(rows):
+    for j in range(rows):
+        if matrix[i][j] > 0:
+            alive_cells += 1
+            sum_active_cells += matrix[i][j]
+print(f"Alive cells: {alive_cells}")
+print(f"Sum: {sum_active_cells}")
+for row in range(rows):
+    print(" ".join(list(map(str, matrix[row]))))
