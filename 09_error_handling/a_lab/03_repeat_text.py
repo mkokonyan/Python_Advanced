1d3b5c50ad25b23a4c87bf6576f75aaf72b30b1e
mkokonyan <mkokonyan@gmail.com>
2021-11-29 21:08:34 +0200

    last commit

--- 09_error_handling/a_lab/03_repeat_text.py
+++ 09_error_handling/a_lab/03_repeat_text.py
@@ -2,6 +2,9 @@ text = input()
 
 try:
     number = int(input())
-    print(text*number)
 except ValueError:
     print("Variable times must be an integer")
+else:
+    print(text*number)
+finally:
+    print("Printed no matter what")
