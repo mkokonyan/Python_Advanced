0e00ed97b6d352525f7f5ba5f3e5b4f54c63eb43
mkokonyan <mkokonyan@gmail.com>
2021-12-03 00:43:51 +0200

    test message

--- 10_exam_prep/05_exam_2020_Oct_24/02_checkmate.py
+++ 10_exam_prep/05_exam_2020_Oct_24/02_checkmate.py
@@ -4,12 +4,70 @@ def is_valid_index(row, col):
     return False
 
 
+def up_vertical_validation(row, col):
+    for r in range(row-1, 0, -1):
+        if is_valid_index(r, col):
+            if board[r][col] == "Q":
+                return False
+            elif board[r][col] == "K":
+                return True
+    return False
+
+
+def down_vertical_validation(row, col):
+    for r in range(row+1, 8):
+        if is_valid_index(r, col):
+            if board[r][col] == "Q":
+                return False
+            elif board[r][col] == "K":
+                return True
+    return False
+
+
+def left_horizontal_validation(row, col):
+    for c in range(col-1, 0, -1):
+        if is_valid_index(row, c):
+            if board[row][c] == "Q":
+                return False
+            elif board[row][c] == "K":
+                return True
+    return False
+
+
+def right_horizontal_validation(row, col):
+    for c in range(col+1, 8):
+        if is_valid_index(row, c):
+            if board[row][c] == "Q":
+                return False
+            elif board[row][c] == "K":
+                return True
+    return False
+
+def down_right_diagonal_validation(row, col):
+    for c in range(col+1, 8):
+        if is_valid_index(row, c):
+            if board[row][c] == "Q":
+                return False
+            elif board[row][c] == "K":
+                return True
+    return False
+
+
 board = []
 queens_coords = []
+valid_coords = []
 for r in range(8):
     board.append(list(input().split()))
     for c in range(8):
         if board[r][c] == "Q":
             queens_coords.append([r, c])
-print(queens_coords)
-print(board)
+
+for i in range(len(queens_coords)):
+    current_row, current_col = queens_coords[i][0], queens_coords[i][1]
+    if up_vertical_validation(current_row, current_col) or down_vertical_validation(current_row, current_col) \
+            or left_horizontal_validation(current_row, current_col) or right_horizontal_validation(current_row, current_col):
+        valid_coords.append(queens_coords[i])
+
+[print(valid_coords[x]) for x in range(len(valid_coords))]
+
+
